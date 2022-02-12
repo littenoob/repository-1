d5ce49a24808c075633ee2d53a375b5a81156b9b
DESKTOP-SMNKPPB\Administrator <1723957366@qq.com>
2022-02-12 12:44:20 +0800

    no message

---
+++ Vue_product/vue_main_product/views/main.vue
@@ -0,0 +1,39 @@
+<template>
+    <el-container>
+        <el-header>
+            <component-header></component-header>
+        </el-header>
+        <el-main>Main</el-main>
+        <el-footer>Footer</el-footer>
+    </el-container>
+</template>
+
+<script>
+import ComponentHeader from '../src/components/ComponentHeader.vue'
+
+export default {
+    name: 'main',
+    components: {
+        ComponentHeader
+    },
+    data() {
+        return {
+        }
+    }
+}
+</script>
+<style lang="less" scoped>
+    body {
+        padding: 0px;
+        margin: 0px;
+    }
+    .el-header{
+        background-color: black;
+        
+    }
+    .el-main{
+        padding-top: 0;
+        height: 700px;
+        background-color: rgb(47, 119, 201);
+    }
+</style>
